22133f3972d788a41c96e302063ed959638ff893
Rrantisi <rantisireham19@gmail.com>
2023-06-06 15:57:59 -0400

    wip: creates recipe model and registers it for admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    region = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
