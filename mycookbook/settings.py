600cbbef221b992424ea920ec88a0fa1d5df9b2e
Rrantisi <rantisireham19@gmail.com>
2023-06-06 14:10:15 -0400

    wip: updates db and urls

--- mycookbook/settings.py
+++ mycookbook/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,11 +73,13 @@ WSGI_APPLICATION = 'mycookbook.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+     'default': { 'ENGINE': 'django.db.backends.postgresql', 'NAME': 'catcollector', }}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mycookbook',
     }
 }
 
