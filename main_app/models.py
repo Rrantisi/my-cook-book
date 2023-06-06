955e4d8c5a6435c01de2c9494c7b93a99ed7e9e7
Rrantisi <rantisireham19@gmail.com>
2023-06-06 16:38:40 -0400

    wip: creates user model and registration form

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
     region = models.CharField(max_length=20, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth import login
+
 
 def home(request):
   return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    # POST request
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) 
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/') #UPDATE IT
+        else:
+            error_message = 'Invalid signup - try again'
+    # GET request
+    form = UserCreationForm() 
+    return render(request, 'registration/signup.html', 
+    {'form': form,
+    'error': error_message}
+    )
